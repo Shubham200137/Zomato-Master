3e4d7f2cec73a3f0a157fd6d8db0b70cad597c45
Shubham200137 <shubhamsharma200137@gmail.com>
2021-12-26 16:02:39 +0530

    Review API

--- API/Reviews/index.js
+++ API/Reviews/index.js
@@ -1,48 +1,44 @@
 import express from "express";
 
-import {UserModel} from "../../database/allModels";
+import {ReviewModel} from "../../database/allModels";
 
 const Router = express.Router();
 
 /*
-Route        /
-Des          Get an user data
-Params       _id
+Route        /new
+Des          Add new review
+Params       none
 Access       Public
-Method       GET
+Method       POST
 */
 
-Router.get("/:_id", async(req,res)=> {
+Router.post("/new", async(req,res)=> {
   try {
-    const {_id} = req.params;
-    const getUser = await UserModel.findById(_id);
-    return res.json({user: getUser});
+    const {reviewData} = req.body;
+
+    await ReviewModel.create(reviewData);
+
+    return res.json({review: "Successfully created review"});
   } catch (error) {
     return res.status(500).json({error: error.message});
   }
 });
 
 /*
-Route        /update
-Des          Update an user data
-Params       userId
+Route        /delete
+Des          Delete a review
+Params       _id
 Access       Public
-Method       PUT
+Method       DELETE
 */
 
-Router.put("/update/:_userId", async(req,res)=> {
+Router.delete("/delete/:_id", async(req,res)=> {
   try {
-    const {userId} = req.params;
-    const {userData} = req.body;
-    const updateUserData = await UserModel.findByIdAndUpdate(
-      userId,
-      {
-        $set: userData
-      },
-      {new: true}
-    );
+    const {_id} = req.params;
 
-    return res.json({user: updateUserData});
+    await ReviewModel.findByIdAndDelete(_id);
+
+    return res.json({review: "Successfully Deleted review"});
   } catch (error) {
     return res.status(500).json({error: error.message});
   }
