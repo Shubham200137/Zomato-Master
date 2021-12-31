5cf70790fbc4387fe6594e2a517a5ccf62e733ea
Shubham200137 <shubhamsharma200137@gmail.com>
2021-12-31 13:46:17 +0530

    image api

--- API/Image/index.js
+++ API/Image/index.js
@@ -2,18 +2,42 @@ import express from "express";
 import AWS from "aws-sdk";
 import multer from "multer";
 
-import {ImageModel} from "../../databases/allModels";
+import {ImageModel} from "../../database/allModels";
 
-const Router= express.Router();
+import {s3Upload} from "../../Utils/AWS/s3";
 
-//Multer CONFIG
-const storage=multer.memoryStorage();
-const upload=multer({storage});
+const Router = express.Router();
+
+//MULTER CONFIG
+const storage = multer.memoryStorage();
+const upload = multer({storage});
+
 
 /*
-Route     /
-Des       Uploading the given image to AWS s3 bucket and then saving the file to mongoDB
-params    None
-Access    Public
-Method    GET
+Route        /
+Des          Uploading the given image to AWS S3 Bucket and then saving the file to mongodb
+Params       None
+Access       Public
+Method       POST
 */
+
+Router.post("/", upload.single("file"), async(req,res)=> {
+  try {
+    const file = req.file;
+    const bucketOptions = {
+      Bucket: "z-master",
+      Key: file.originalname,
+      Body: file.buffer,
+      ContentType: file.mimetype,
+      ACL: "public-read"
+    };
+
+    const uploadImage = await s3Upload(bucketOptions);
+    return res.status(200).json({uploadImage});
+
+  } catch (error) {
+    return res.status(500).json({error: error.message});
+  }
+});
+
+export default Router;
